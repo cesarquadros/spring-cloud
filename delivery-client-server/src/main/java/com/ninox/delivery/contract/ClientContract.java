256fb9c3ccefbf730f65a5121602e943b3531bf3
Cesar Quadros <cesar.quadros@viavarejo.com>
2018-08-23 17:46:51 -0300

    implementação do zuul

--- delivery-client-server/src/main/java/com/ninox/delivery/contract/ClientContract.java
+++ delivery-client-server/src/main/java/com/ninox/delivery/contract/ClientContract.java
@@ -3,6 +3,9 @@ package com.ninox.delivery.contract;
 import java.io.IOException;
 import java.util.List;
 
+import javax.servlet.http.HttpServletResponse;
+import javax.ws.rs.core.Context;
+
 import org.springframework.http.HttpEntity;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -10,21 +13,29 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.ninox.delivery.document.Client;
 import com.ninox.delivery.response.Response;
 
 public interface ClientContract {
 
-	@GetMapping(value = "/api/client", produces = MediaType.APPLICATION_JSON_VALUE)
+	@GetMapping(value = "/client", produces = MediaType.APPLICATION_JSON_VALUE)
 	ResponseEntity<Response<List<Client>>> listarTodos();
 	
-	@PostMapping(value = "/api/client", consumes =MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+	@PostMapping(value = "/client", consumes =MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
 	ResponseEntity<Response<Client>> cadastrar(@RequestBody Client client);
 	
-	@GetMapping(value = "api/client/sendemail/{id}", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+	@GetMapping(value = "/client/sendemail/{id}", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
 	Response<String> sendEmail(@PathVariable(name="id")String id);
 	
-	@GetMapping(value = "/api/client/downloadcsv/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+	@GetMapping(value = "/client/downloadcsv/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
 	HttpEntity<byte[]> download(@PathVariable(name="id")String id) throws IOException;
+	
+	@GetMapping(value = "/client/downloadarquivo", produces = MediaType.APPLICATION_JSON_VALUE)
+	HttpEntity<byte[]> downloadArquivo(@RequestParam(value = "cnpj") String cnpj, @RequestParam(value = "nomeArquivo") String nomeArquivo) throws IOException;
+	
+	@GetMapping(value="/client/downcsv/{id}", produces = MediaType.APPLICATION_OCTET_STREAM_VALUE)
+	public ResponseEntity downloadCsv(@Context HttpServletResponse response, @PathVariable(name="id")String id);
+	
 }
--- delivery-client-server/src/main/java/com/ninox/delivery/rest/ClientController.java
+++ delivery-client-server/src/main/java/com/ninox/delivery/rest/ClientController.java
@@ -1,11 +1,17 @@
 package com.ninox.delivery.rest;
 
 import java.io.IOException;
+import java.io.OutputStreamWriter;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.servlet.http.HttpServletResponse;
+import javax.ws.rs.core.Context;
+
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVPrinter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
@@ -13,6 +19,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.util.ObjectUtils;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ninox.delivery.contract.ClientContract;
@@ -23,14 +30,14 @@ import com.wordnik.swagger.annotations.Api;
 
 @Api(value = "TESTE", tags = "Teste")
 @RestController
-public class ClientController implements ClientContract{
+public class ClientController implements ClientContract {
 
 	@Autowired
 	private ClientServiceImpl clientServiceImpl;
-	
+
 	@Override
 	public ResponseEntity<Response<List<Client>>> listarTodos() {
-		
+
 		return ResponseEntity.ok(new Response<List<Client>>(clientServiceImpl.listarClientes()));
 	}
 
@@ -40,34 +47,99 @@ public class ClientController implements ClientContract{
 	}
 
 	@Override
-	public HttpEntity<byte[]> download(@PathVariable(name="id")String id) throws IOException {
-		
+	public HttpEntity<byte[]> download(@PathVariable(name = "id") String id) throws IOException {
+
 		String nomeArquivo = this.clientServiceImpl.gerarCsv(this.clientServiceImpl.clientById(id).get());
-		
-		byte[] arquivo = Files.readAllBytes( Paths.get("./" + nomeArquivo));
+
+		byte[] arquivo = Files.readAllBytes(Paths.get("./" + nomeArquivo));
 		HttpHeaders httpHeaders = new HttpHeaders();
-		
+
 		httpHeaders.add("Content-Disposition", "attachment;filename=\"" + nomeArquivo + "\"");
-		
+
 		return new HttpEntity<>(arquivo, httpHeaders);
-		
+
 	}
 
 	@Override
-	public Response<String> sendEmail(@PathVariable(name="id")String id) {
-		
+	public Response<String> sendEmail(@PathVariable(name = "id") String id) {
+
 		List<String> mensagem = new ArrayList<>();
-		
+
 		Client client = clientServiceImpl.clientById(id).get();
-		
+
 		Response<String> response = new Response<>(null);
-		
-		if(!ObjectUtils.isEmpty(client)) {
+
+		if (!ObjectUtils.isEmpty(client)) {
 			mensagem.add(clientServiceImpl.sendEmail(client));
 		}
-		
+
 		response.setErros(mensagem);
-		
+
 		return response;
 	}
+
+	@Override
+	public HttpEntity<byte[]> downloadArquivo(@RequestParam(value = "cnpj") String cnpj,
+			@RequestParam(value = "nomeArquivo") String nomeArquivo) throws IOException {
+
+		byte[] arquivo = Files.readAllBytes(Paths.get("/opt/upload/in/teste.pdf"));
+
+		HttpHeaders httpHeaders = new HttpHeaders();
+
+		httpHeaders.add("Content-Disposition", "attachment;filename=\"" + nomeArquivo + "\"");
+
+		return new HttpEntity<>(arquivo, httpHeaders);
+	}
+
+	@Override
+	public ResponseEntity downloadCsv(@Context HttpServletResponse response, @PathVariable(name = "id") String id) {
+
+		try {
+
+			List<Client> list = new ArrayList<>();
+
+			list.add(this.clientServiceImpl.clientById(id).get());
+
+			// setting the content disposition
+			response.setHeader("Content-Disposition",
+					"attachment; filename=" + "report_" + System.currentTimeMillis() + ".csv");
+			// marking content type as csv
+			response.setContentType("text/csv");
+			// retrieving output stream writer from the response
+			OutputStreamWriter osw = new OutputStreamWriter(response.getOutputStream(), "UTF-8");
+
+			// Initializing Apache Commons Csv format
+
+			// Create the CSVFormat object with "\n" as a record delimiter
+			CSVFormat csvFileFormat = CSVFormat.DEFAULT.withDelimiter(',').withRecordSeparator("\n");
+
+			// Initializing Apache Commons Csv printer
+
+			CSVPrinter csvFilePrinter = new CSVPrinter(osw, csvFileFormat);
+
+			// printing header to the csv file
+			csvFilePrinter.printRecord(Client.class);
+
+			for (Client client : list) {
+				List<String> row = new ArrayList<String>();
+				// adding columns
+				row.add(client.getId());
+				row.add(client.getNome());
+				// printing rows
+				csvFilePrinter.printRecord(row);
+			}
+
+			csvFilePrinter.flush();
+			csvFilePrinter.close();
+
+			osw.flush();
+			osw.close();
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		return ResponseEntity.ok().build();
+	}
+
 }
